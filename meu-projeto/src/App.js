6712c97652862c43f0802afe5fe39f9e6db99c91
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-07-16 16:06:56 -0300

    Update App.js

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 function App() {
   let name = 'Gustavo'
+  let NewName = name.toUpperCase()
+  let url = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.quizur.com%2Fquiz%2Fquao-sigma-voce-e-Rjfh&psig=AOvVaw2GcYJdhuLPyd22wtwziDIu&ust=1752778645218000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPD9srqHwo4DFQAAAAAdAAAAABAE'
+
+  function sum(a, b){
+    return a + b
+  }
   return (
-    <div>
-      <h1>Olá, {name}</h1>
+    <div className='App'>
+      <h1>Olá, {NewName}</h1>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt='Sigma Imagem'/>
     </div>
   );
 }
