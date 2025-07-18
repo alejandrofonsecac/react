ddfcfc92a9b9600c065d7273f5adcf8eabdbf039
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-07-18 19:46:43 -0300

    Rook em form

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,16 +1,38 @@
+import { useState } from "react"
+
 function Form(){
 
     function cadastrarUsuario(event){
         event.preventDefault()
+        console.log(`Usuario ${name} foi cadastrado com a senha: ${password}`)
+        window.alert(name)
         window.alert('Cadastrou o usuário!')
     }
+
+    const [name, setName] = useState('Matheus')
+    const [password, setPassword] = useState()
 
     return(
         <div>
             <h1>Meu Cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" />
+                    <input type="text" 
+                    id="name" 
+                    name="name" 
+                    placeholder="Digite seu nome" 
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Digite sua senha"
+                    onChange={(event) => setPassword(event.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value='Cadastrar' />
