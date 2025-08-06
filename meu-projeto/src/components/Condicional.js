c23085f2b34f2de2179a614d807d9843d0f83b98
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-08-06 14:49:17 -0300

    Renderizando Condicional

--- meu-projeto/src/components/Condicional.js
+++ meu-projeto/src/components/Condicional.js
@@ -8,17 +8,22 @@ function Condicional(){
     function enviarEmail(e){
         e.preventDefault()
         setUserEmail(email)
-        console.log(email)
+        console.log(userEmail)
     }
 
     return(
         <div>
             <h2>Cadastre o seru email</h2>
             <form>
-                <input type='email' placeholder='Digite o seu email...'/>
+                <input type='email' placeholder="Digite o seu email..." onChange={(e) => setEmail(e.target.value)}/>
                 <button type='submit' onClick={enviarEmail}>
                     Enviar Email
                 </button>
+                {userEmail && (//Essa linha vai fazer o que esta entre chaves se for TRUE
+                    <div>
+                        <p>O email do usuário é: {userEmail}</p>
+                    </div>
+                )}
             </form>           
         </div>
     )
